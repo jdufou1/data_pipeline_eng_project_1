7866024d4cb664c131b349ed868215705224a619
jdufou1 <jeremy.dufourmantelle@gmail.com>
2023-11-18 11:04:20 +0100

    schedule every hours and scheduled task put source data into another bucket after using it

--- dags/dag_pipeline.py
+++ dags/dag_pipeline.py
@@ -25,7 +25,7 @@ with DAG(
     description="send flashscore data from GCS to Snowflake",
     start_date= pendulum.datetime(2023, 1, 1, tz="UTC"), # pendulum.datetime(2023, 11, 18, tz="UTC"), # start_date,
     catchup=False,
-    schedule_interval="*/2 * * * *"# "50 23 * * *"
+    schedule_interval="0 * * * *"# "50 23 * * *"
 ) as dag :
     
     starting_task = BashOperator(
@@ -104,9 +104,6 @@ with DAG(
             # Créer un curseur
             cur = conn.cursor()
 
-            
-
-
             # Requête d'insertion
             insert_query = f"INSERT INTO {NAME_TABLE} (COUNTRY_MATCH, TOURNAMENT, NAME_TEAM_HOME, SCORE_TEAM_HOME, NAME_TEAM_AWAY, SCORE_TEAM_AWAY) VALUES (%s, %s, %s, %s, %s, %s)"
 
@@ -125,9 +122,12 @@ with DAG(
             print("fermeture")
             
         def ETL_pipeline(list_files):
+            bucket_source = "data-flashscore"
+            bucket_destination = "data-flashscore-used"
             for file in list_files:
                 # Appel de la fonction
                 json_obj = extract(file)
+                move_file(bucket_source, file, bucket_destination, file)
                 df = transform_pandas(json_obj)
                 load(df)
 
@@ -147,10 +147,10 @@ with DAG(
             today = datetime.now().date()
             yesterday = today - timedelta(days=1)
 
-            blobs_today_and_yesterday = [blob for blob in bucket.list_blobs()
-                                        if blob.updated.date() == today or blob.updated.date() == yesterday]
+            # blobs_today_and_yesterday = [blob for blob in bucket.list_blobs()
+            #                             if blob.updated.date() == today or blob.updated.date() == yesterday]
 
-            file_names_today_and_yesterday = [blob.name for blob in blobs_today_and_yesterday]
+            file_names_today_and_yesterday = [blob.name for blob in bucket.list_blobs() ]# blobs_today_and_yesterday]
 
             return file_names_today_and_yesterday
 
@@ -193,3 +193,34 @@ def read_json_from_gcs(bucket_name, file_path):
     data = json.loads(json_content)
 
     return data
+
+
+def move_file(source_bucket_name, source_object_name, destination_bucket_name, destination_object_name):
+    credentials_json_str = os.getenv('GOOGLE_APPLICATION_CREDENTIALS_JSON')
+    # Vérifiez si la variable d'environnement est définie
+    if credentials_json_str is None:
+        raise ValueError("La variable d'environnement GOOGLE_APPLICATION_CREDENTIALS_JSON n'est pas définie.")
+
+    # Chargez la clé JSON depuis la chaîne JSON
+    credentials = json.loads(credentials_json_str)
+
+    client = storage.Client.from_service_account_info(credentials)
+
+    # Récupérer le seau source
+    source_bucket = client.bucket(source_bucket_name)
+
+    # Récupérer l'objet source
+    source_blob = source_bucket.blob(source_object_name)
+
+    # Récupérer le seau de destination
+    destination_bucket = client.bucket(destination_bucket_name)
+
+    # Copier l'objet vers le seau de destination
+    source_bucket.copy_blob(
+        source_blob, destination_bucket, destination_object_name
+    )
+
+    # Supprimer l'objet source après avoir été copié
+    source_blob.delete()
+
+    print(f"Fichier déplacé avec succès de {source_bucket_name}/{source_object_name} vers {destination_bucket_name}/{destination_object_name}")
--- main.py
+++ main.py
@@ -33,28 +33,67 @@ load_dotenv()
 
 def get_files_published_today(bucket_name):
             
+    credentials_json_str = os.getenv('GOOGLE_APPLICATION_CREDENTIALS_JSON')
+    # Vérifiez si la variable d'environnement est définie
+    if credentials_json_str is None:
+        raise ValueError("La variable d'environnement GOOGLE_APPLICATION_CREDENTIALS_JSON n'est pas définie.")
 
+    # Chargez la clé JSON depuis la chaîne JSON
+    credentials = json.loads(credentials_json_str)
 
-            credentials_json_str = os.getenv('GOOGLE_APPLICATION_CREDENTIALS_JSON')
-            # Vérifiez si la variable d'environnement est définie
-            if credentials_json_str is None:
-                raise ValueError("La variable d'environnement GOOGLE_APPLICATION_CREDENTIALS_JSON n'est pas définie.")
+    client = storage.Client.from_service_account_info(credentials)
 
-            # Chargez la clé JSON depuis la chaîne JSON
-            credentials = json.loads(credentials_json_str)
+    bucket = client.get_bucket(bucket_name)
 
-            client = storage.Client.from_service_account_info(credentials)
+    today = datetime.now().date()
+    yesterday = today - timedelta(days=1)
 
-            bucket = client.get_bucket(bucket_name)
+    blobs_today_and_yesterday = [blob for blob in bucket.list_blobs()
+                                if blob.updated.date() == today or blob.updated.date() == yesterday]
 
-            today = datetime.now().date()
-            yesterday = today - timedelta(days=1)
+    file_names_today_and_yesterday = [blob.name for blob in blobs_today_and_yesterday]
 
-            blobs_today_and_yesterday = [blob for blob in bucket.list_blobs()
-                                        if blob.updated.date() == today or blob.updated.date() == yesterday]
-
-            file_names_today_and_yesterday = [blob.name for blob in blobs_today_and_yesterday]
-
-            return file_names_today_and_yesterday
+    return file_names_today_and_yesterday
 
 print(get_files_published_today("data-flashscore"))
+
+
+
+def move_file(source_bucket_name, source_object_name, destination_bucket_name, destination_object_name):
+    credentials_json_str = os.getenv('GOOGLE_APPLICATION_CREDENTIALS_JSON')
+    # Vérifiez si la variable d'environnement est définie
+    if credentials_json_str is None:
+        raise ValueError("La variable d'environnement GOOGLE_APPLICATION_CREDENTIALS_JSON n'est pas définie.")
+
+    # Chargez la clé JSON depuis la chaîne JSON
+    credentials = json.loads(credentials_json_str)
+
+    client = storage.Client.from_service_account_info(credentials)
+
+    # Récupérer le seau source
+    source_bucket = client.bucket(source_bucket_name)
+
+    # Récupérer l'objet source
+    source_blob = source_bucket.blob(source_object_name)
+
+    # Récupérer le seau de destination
+    destination_bucket = client.bucket(destination_bucket_name)
+
+    # Copier l'objet vers le seau de destination
+    new_blob = source_bucket.copy_blob(
+        source_blob, destination_bucket, destination_object_name
+    )
+
+    # Supprimer l'objet source après avoir été copié
+    source_blob.delete()
+
+    print(f"Fichier déplacé avec succès de {source_bucket_name}/{source_object_name} vers {destination_bucket_name}/{destination_object_name}")
+
+
+
+bucket_source = "data-flashscore"
+bucket_destination = "data-flashscore-used"
+file = "04_11_2023-18_38_54.json"
+
+# Exemple d'utilisation
+move_file(bucket_source, file, bucket_destination, file)
